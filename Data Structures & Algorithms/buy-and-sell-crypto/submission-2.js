cf3a7f40495f9ae195ea09697dabade880945e25
shivanand-aradhya <76728168+shivanand-aradhya@users.noreply.github.com>
2026-04-28 15:41:28 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let max = 0;
+        let l=0;
+        let r=1;
+        while(r<prices.length) {
+            if(prices[l] > prices[r]) {
+                l=r;
+            } else {
+                let profit = prices[r] - prices[l];
+                max = Math.max(max, profit);
+            }
+            r++;
+        }
+        return max;
+    }
+}
